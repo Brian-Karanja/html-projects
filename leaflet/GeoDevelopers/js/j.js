b9b10a735ab1851f52e8898f4798209ebc0c113c
brian <brianmugambikaranja@gmail.com>
2018-01-18 22:30:08 +0300

    all my html projects

---
+++ leaflet/GeoDevelopers/js/j.js
@@ -0,0 +1,16 @@
+	$(document).ready(function(){
+			// document.write('Hello world');
+			$('.me').css('background-color','lime')
+			$('.me').css('color','red')
+			$('div').addClass('selected')
+			$('img').attr({
+				src: 'images/logo.png',
+				title: 'Jquery',
+				alt: 'Jquery Logo'
+			});
+
+			var inte = ['ayub','kevo','jame','sly','kim']; 
+			for(var i=0; i<inte.length;i++){
+				// alert(i);
+			}
+	});
